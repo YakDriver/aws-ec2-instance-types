6f1807d2ef7209da242c0b985094ab0a73f03b43
Dirk Avery <dirk.avery@gmail.com>
2020-08-05 20:22:25 -0400

    Fix table issue

--- get_instance_types.sh
+++ get_instance_types.sh
@@ -114,9 +114,8 @@ function instance_types() {
     printf "| Instance Type | ${region_id}-az%d | ${region_id}-az%d | ${region_id}-az%d | ${region_id}-az%d |\n" {1..4} >> "${output}"
     printf "| ------------- | :-----------: | :-----------: | :-----------: | :-----------: |\n" >> "${output}"
     for type in "${types[@]}"; do
-      row="| ${class}.${type} |"
+      local row="| ${class}.${type} |"
       local include_row=0
-      local row=""
       for i in {1..4}
       do
         local az_id="${region_id}-az${i}"
